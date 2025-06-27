58628bf15f6bcf9dd6ed1d695926d52ca9b6ff81
rjsudhin <rjsudhin@users.noreplay.githut.com>
2025-06-27 13:29:26 +0530

    it' working with localStorage

--- js/app.js
+++ js/app.js
@@ -1,24 +1,30 @@
 // Note keeper
+const addedNoteSectionContainer = document.querySelector('#added-note-section-container')
+
+// checking the localStorage is empty or not
+if (localStorage.length > 0) {
+  gettingNotes()
+}
 
 const noteTitle = document.querySelector('#title')
 const noteContent = document.querySelector('#content')
 const noteAddBtn = document.querySelector('.add-note-btn')
 
 
+
 // adding new Notes
 noteAddBtn.addEventListener('click', () => {
   let userNoteTitle = noteTitle.value 
   let userNoteContent = noteContent.value
+
   if (!userNoteTitle == '' && !userNoteContent == '') {
     localStorage.setItem(userNoteTitle, userNoteContent)
     noteTitle.value = ''
     noteContent.value = ''
     noteTitle.focus()
-
   } else {
     console.log('something add in input fields')
   }
-
 
   // make new notes UI
   addingNewNotes()
@@ -26,5 +32,30 @@ noteAddBtn.addEventListener('click', () => {
 
 
 function addingNewNotes() {
+  // getting things from localStorage
+  gettingNotes()
+  
+}
 
+
+function gettingNotes() {
+  addedNoteSectionContainer.innerHTML = ''
+  let checkingProps = localStorage
+  for (let prop in checkingProps) {
+    if (prop != 'length' && typeof checkingProps[prop] != 'function') {
+      let noteCard = document.createElement('div')
+      noteCard.classList.add('note-card')
+
+      let cardTitle = document.createElement('h3')
+      cardTitle.textContent = prop
+      cardTitle.classList.add('note-title')
+
+      let cardContent = document.createElement('p')
+      cardContent.textContent = checkingProps[prop]
+      cardContent.classList.add('note-content')
+
+      noteCard.append(cardTitle, cardContent)
+      addedNoteSectionContainer.appendChild(noteCard)
+    }
+  }
 }
