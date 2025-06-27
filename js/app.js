2ca6212030651cb186d4591786ee9084ca78af46
rjsudhin <rjsudhin@users.noreplay.githut.com>
2025-06-28 02:23:24 +0530

    added notes and it will now showing to screen

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ const addedNoteSectionContainer = document.querySelector('#added-note-section-co
 // checking the localStorage is empty or not
 if (localStorage.length > 0) {
   gettingNotes()
+  addedNoteSectionContainer.classList.add('added-note-section-container')
 }
 
 const noteTitle = document.querySelector('#title')
@@ -28,6 +29,7 @@ noteAddBtn.addEventListener('click', () => {
 
   // make new notes UI
   addingNewNotes()
+  addedNoteSectionContainer.classList.add('added-note-section-container')
 })
 
 
@@ -43,18 +45,37 @@ function gettingNotes() {
   let checkingProps = localStorage
   for (let prop in checkingProps) {
     if (prop != 'length' && typeof checkingProps[prop] != 'function') {
+      // card container
       let noteCard = document.createElement('div')
       noteCard.classList.add('note-card')
-
+      // header components
+      let headComponents = document.createElement('div')
+      headComponents.classList.add('head-components')
+      // icons
+      let pinIcon = document.createElement('span')
+      pinIcon.classList.add('material-symbols-outlined')
+      pinIcon.textContent = 'keep'
+      // title
       let cardTitle = document.createElement('h3')
       cardTitle.textContent = prop
       cardTitle.classList.add('note-title')
+
+      headComponents.append(pinIcon, cardTitle)
+
+      let otherComponents = document.createElement('div')
+      otherComponents.classList.add('other-components')
 
       let cardContent = document.createElement('p')
       cardContent.textContent = checkingProps[prop]
       cardContent.classList.add('note-content')
 
-      noteCard.append(cardTitle, cardContent)
+      let deletbtn = document.createElement('span')
+      deletbtn.classList.add('material-symbols-outlined')
+      deletbtn.textContent = 'delete'
+
+      otherComponents.append(cardContent, deletbtn)
+
+      noteCard.append(headComponents, otherComponents)
       addedNoteSectionContainer.appendChild(noteCard)
     }
   }
